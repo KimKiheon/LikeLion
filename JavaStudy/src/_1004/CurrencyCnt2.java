0b5cfb5ec48a565adb80f68dfa5969d9523e3e59
Kimkiheon <cbj2741@gmail.com>
2022-10-04 14:26:26 +0900

    Curren2

--- JavaStudy/src/_1004/CurrencyCnt2.java
+++ JavaStudy/src/_1004/CurrencyCnt2.java
@@ -9,13 +9,13 @@ public class CurrencyCnt2 {
         int curren10000=10000;
         int curren5000=5000;
         int curren1000=1000;
-        System.out.printf("50000원 :%d\n",refund/curren50000);
+        System.out.printf("50000원 :%d 나머지 %d\n",refund/curren50000,refund%curren50000);
         refund%=curren50000;
-        System.out.printf("10000원 :%d\n",refund/curren10000);
+        System.out.printf("10000원 :%d 나머지 %d\n",refund/curren10000,refund%curren10000);
         refund%=curren10000;
-        System.out.printf("5000원 :%d\n",refund/curren5000);
+        System.out.printf("5000원 :%d 나머지 %d\n",refund/curren5000,refund%curren5000);
         refund%=curren5000;
-        System.out.printf("1000원 :%d\n",refund/curren1000);
+        System.out.printf("1000원 :%d 나머지 %d\n",refund/curren1000,refund%curren1000);
         refund%=curren1000;
 
 
