fe72b1077cb10b73b295597a2964898b28a89e5d
Kimkiheon <cbj2741@gmail.com>
2022-10-04 13:20:53 +0900

    arraytest

---
+++ JavaStudy/src/_1004/ArrayTest.java
@@ -0,0 +1,10 @@
+package _1004;
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        for (int i = 0; i < 10; i++) arr[i] = i;
+        for (int i = 0; i < 10; i++) System.out.println(arr[i]);
+
+    }
+}
