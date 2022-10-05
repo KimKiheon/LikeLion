c9ec5bdbd1799a4097fb9f9060302532d48f4c7d
Kimkiheon <cbj2741@gmail.com>
2022-10-05 10:32:35 +0900

    calculator

---
+++ JavaStudy/src/_1005/Calculator.java
@@ -0,0 +1,21 @@
+package _1005;
+
+public class Calculator {
+    public void plus(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public void minus(int a, int b) {
+        System.out.println(a - b);
+    }
+
+    public void multiple(int a, int b) {
+        System.out.println(a * b);
+    }
+
+    public void divide(int a, int b) {
+        double result = (double) a / b;
+        System.out.println(result);
+    }
+
+}
---
+++ JavaStudy/src/_1005/CalculatorMain.java
@@ -0,0 +1,9 @@
+package _1005;
+
+public class CalculatorMain {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.divide(10,20);
+
+    }
+}
