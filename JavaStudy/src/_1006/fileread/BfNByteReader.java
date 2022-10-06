aecb31a9980c7a477573293b62fa2e6ffcc07851
Kimkiheon <cbj2741@gmail.com>
2022-10-06 15:14:32 +0900

    File목록

--- JavaStudy/src/_1006/fileread/BfNByteReader.java
+++ JavaStudy/src/_1006/fileread/BfNByteReader.java
@@ -1,4 +1,4 @@
-package _1006.file;
+package _1006.fileread;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
--- JavaStudy/src/_1006/fileread/Main.java
+++ JavaStudy/src/_1006/fileread/Main.java
@@ -1,9 +1,17 @@
-package _1006.file;
+package _1006.fileread;
 
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for(File file: files){
+            System.out.println(file);
+        }
+
+
         BfNByteReader bfNByteReader1 = new BfNByteReader("JavaStudy/src/_1006/textfile/hi.txt",1);
         System.out.println("1글자 읽기");
         bfNByteReader1.bufferedReader();
@@ -16,5 +24,25 @@ public class Main {
         System.out.println("N글자 읽기");
         bfNByteReader3.bufferedReader();
     }
+/*
+C:\Dev\jdk-11.0.15.1\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.2\lib\idea_rt.jar=61322:C:\Program Files\JetBrains\IntelliJ IDEA 2022.2.2\bin" -Dfile.encoding=UTF-8 -classpath C:\Dev\LikeLion\out\production\LikeLion _1006.fileread.Main
+.\.git
+.\.gitignore
+.\.idea
+.\Hi.txt
+.\JavaStudy
+.\LikeLion.iml
+.\out
+.\README.md
+1글자 읽기
+H
+2글자 읽기
+H
+e
+N글자 읽기
+H
+e
+l
 
+ */
 }
