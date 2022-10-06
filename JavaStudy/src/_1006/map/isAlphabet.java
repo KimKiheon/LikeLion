715d08706e2edc165de38e9066251d87efcfbe82
Kimkiheon <cbj2741@gmail.com>
2022-10-06 13:20:48 +0900

    알파벳 카운팅

--- JavaStudy/src/_1006/map/isAlphabet.java
+++ JavaStudy/src/_1006/map/isAlphabet.java
@@ -1,17 +1,11 @@
 package _1006.map;
 
 public class isAlphabet {
-    private char c;
-    public isAlphabet(char c){
-        this.c=c;
-    }
-    public boolean alphabetDecide(){
-        if(65<=c&&c<=90){
+
+    public boolean isAlphabet(char c) {
+        if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')) {
             return true;
-        }else if(97<=c&&c<=122) {
-            return true;
-        }else{
+        } else
             return false;
-        }
     }
 }
