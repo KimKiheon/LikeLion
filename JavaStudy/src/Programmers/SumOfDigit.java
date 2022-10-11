2bde70ea474768a08b958e76231b41143336c606
Kimkiheon <cbj2741@gmail.com>
2022-10-11 13:09:43 +0900

    Programmers 자릿수 더하기

---
+++ JavaStudy/src/Programmers/SumOfDigit.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+public class SumOfDigit {
+    public int solution(int n) {
+        int answer = 0;
+        while (0 < n) {
+            answer += n % 10;
+            n /= 10;
+        }
+        System.out.println(answer);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SumOfDigit sod = new SumOfDigit();
+        int result1 = sod.solution(1234);
+
+        if(result1==10){
+            System.out.printf("정답");
+        }
+        else{
+            System.out.printf("실패");
+        }
+    }
+}
