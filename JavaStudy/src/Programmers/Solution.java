b179a936e21a2515e2e20df38fcb27735e5dd41d
Kimkiheon <cbj2741@gmail.com>
2022-10-11 09:15:46 +0900

    Programmers 자릿수 더하기

---
+++ JavaStudy/src/Programmers/Solution.java
@@ -0,0 +1,14 @@
+package Programmers;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(0<n){
+            answer+=n%10;
+            n/=10;
+        }
+        System.out.println(answer);
+
+        return answer;
+    }
+}
