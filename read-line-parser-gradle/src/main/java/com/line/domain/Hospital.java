b47f728830213def21a47a2c22e48a1130f27828
Kimkiheon <cbj2741@gmail.com>
2022-10-13 13:12:32 +0900

    HospitalParse Test parsing

--- read-line-parser-gradle/src/main/java/com/line/domain/Hospital.java
+++ read-line-parser-gradle/src/main/java/com/line/domain/Hospital.java
@@ -3,11 +3,35 @@ package com.line.domain;
 public class Hospital {
     private String id, address, district, category, name, subdivision;
     private int emergencyRoom;
-    public Hospital(String id){
-        this.id=id.replaceAll("\"","");
+
+    public Hospital(String id) {
+        this.id = id;
+    }
+
+    public Hospital(String id, String address) {
+        this.id = id;
+        this.address = address;
     }
 
     public String getId() {
         return id;
+    }
+    public int getEmergencyRoom() {
+        return emergencyRoom;
+    }
+    public String getSubdivision() {
+        return subdivision;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public String getDistrict() {
+        return district;
+    }
+    public String getAddress() {
+        return address;
     }
 }
