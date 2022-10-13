fa5844946eac59c4c1984bebe8df1ff5752c0da6
Kimkiheon <cbj2741@gmail.com>
2022-10-13 10:19:02 +0900

    Bubble Sort

--- JavaStudy/src/_1013/BubbleSort.java
+++ JavaStudy/src/_1013/BubbleSort.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class BubbleSort {
     public int[] Bubble(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i + 1; j < 6; j++) {
+            for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] > arr[j]) {
                     int temp = arr[i];
                     arr[i] = arr[j];
