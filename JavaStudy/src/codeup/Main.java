d95ece92d721a965aad022fd4e15529de0e2be6c
Kimkiheon <cbj2741@gmail.com>
2022-10-14 11:09:14 +0900

    Hospital Test Query

--- JavaStudy/src/codeup/Main.java
+++ JavaStudy/src/codeup/Main.java
@@ -6,11 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a,b;
-        a=sc.nextInt();
-        b=sc.nextInt();
-
-        System.out.println(a&b);
+        int a = sc.nextInt();
+        switch (a) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("winter");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("summer");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("fall");
+                break;
+        }
     }
 }
 
