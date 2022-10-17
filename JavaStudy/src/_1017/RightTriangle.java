f015eb884a828b20df53d585623f86075c4c435d
Kimkiheon <cbj2741@gmail.com>
2022-10-17 09:21:53 +0900

    HospitalParser

---
+++ JavaStudy/src/_1017/RightTriangle.java
@@ -0,0 +1,12 @@
+package _1017;
+
+public class RightTriangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
