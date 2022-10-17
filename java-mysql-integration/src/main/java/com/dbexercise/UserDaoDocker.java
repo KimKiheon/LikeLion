753c7b02c7f036632796643bb2d8dddf41b14bea
Kimkiheon <cbj2741@gmail.com>
2022-10-17 13:30:19 +0900

    Select query

--- java-mysql-integration/src/main/java/com/dbexercise/UserDaoDocker.java
+++ java-mysql-integration/src/main/java/com/dbexercise/UserDaoDocker.java
@@ -1,9 +1,6 @@
 package com.dbexercise;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Map;
 
 public class UserDaoDocker {
@@ -13,16 +10,25 @@ public class UserDaoDocker {
         String dbUser = env.get("DB_USER");
         String dbPassword = env.get("DB_PASSWORD");
 
-        Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?,?,?)");
-        ps.setString(1,"3");
-        ps.setString(2,"aftertruncate");
-        ps.setString(3,"1234");
+        Connection conn = DriverManager.getConnection(dbHost, dbUser, dbPassword);
+        //PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES (?,?,?)");
+        //ps.setString(1,"6");
+        //ps.setString(2,"aftertruncate");
+        //ps.setString(3,"1234");
+        Statement stmt = conn.createStatement();
+        ResultSet rs;
+        rs = stmt.executeQuery("SELECT *FROM users");
+        while (rs.next()) {
+            String id = rs.getString("id");
+            String name = rs.getString("name");
+            String password = rs.getString("password");
 
+            System.out.println(id + " " + name + " " + password);
+        }
         //int status = ps.executeUpdate();
         //System.out.println(status);
-        ps.executeUpdate();
-        ps.close();
+        //ps.executeUpdate();
+        //ps.close();
         conn.close();
         System.out.println("DB Insert Sueccess");
     }
