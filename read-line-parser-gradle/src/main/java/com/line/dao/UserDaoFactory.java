c523777a6bedad9de1a221f6051d27a5cce9c735
Kimkiheon <cbj2741@gmail.com>
2022-10-19 18:02:28 +0900

    Spring

--- read-line-parser-gradle/src/main/java/com/line/dao/UserDaoFactory.java
+++ read-line-parser-gradle/src/main/java/com/line/dao/UserDaoFactory.java
@@ -1,11 +1,17 @@
 package com.line.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
+    @Bean
     public UserDao awsUserDao(){
         AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
         return userDao;
     }
+    @Bean
     public UserDao localUserDao(){
         UserDao userDao = new UserDao(new LocalConnectionMaker());
         return userDao;
--- read-line-parser-gradle/src/test/java/dao/UserDaoTest.java
+++ read-line-parser-gradle/src/test/java/dao/UserDaoTest.java
@@ -2,23 +2,32 @@ package dao;
 
 import com.line.dao.AWSUserDaoImpl;
 import com.line.dao.UserDao;
+import com.line.dao.UserDaoFactory;
 import com.line.domain.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = UserDaoFactory.class)
 class UserDaoTest {
+    @Autowired
+    ApplicationContext context;
 
     @Test
     void addAndSelect() throws SQLException {
-        UserDao userDao = new UserDao();
-        String id = "19";
-        User user = new User(id, "EternityHwan", "1123");
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
+        String id = "21";
+        User user = new User(id, "spring", "1123");
         userDao.add(user);
 
         User selectedUser = userDao.findById(id);
-        Assertions.assertEquals("EternityHwan", selectedUser.getName());
+        Assertions.assertEquals("spring", selectedUser.getName());
         Assertions.assertEquals("1123", selectedUser.getPassword());
     }
 }
