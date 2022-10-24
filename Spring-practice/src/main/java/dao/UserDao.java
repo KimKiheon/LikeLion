b4909b061022359b696c7ee74aa217edda8c90c0
Kimkiheon <cbj2741@gmail.com>
2022-10-24 11:58:36 +0900

    DataSource 적용

--- Spring-practice/src/main/java/dao/UserDao.java
+++ Spring-practice/src/main/java/dao/UserDao.java
@@ -2,34 +2,28 @@ package dao;
 
 import domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
-import java.util.Map;
 
 public class UserDao {
-    private ConnectionMaker connectionMaker;
-
-    public UserDao() {
-        this.connectionMaker = new AwsConnectionMaker();
+    private final DataSource dataSource;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
-
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public void jdbcContextWithStatementStrategy(StatementStrategy st) throws SQLException {
+        Connection c = dataSource.getConnection();
+        PreparedStatement ps = st.makePreparedStatement(c);
+        ps.executeUpdate();
+        ps.close();
+        c.close();
     }
-
-
-    public void add(User user) {
-        try {
-            jdbcContextWithStatementStrategy(new AddStrategy(user));
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public void add(User user) throws SQLException {
+        jdbcContextWithStatementStrategy(new AddStrategy(user));
     }
-
     public User findById(String id) {
         Connection c;
         try {
-            c = connectionMaker.connectionMaker();
+            c = dataSource.getConnection();
 
             PreparedStatement ps = c.prepareStatement("SELECT * FROM users WHERE id = ?");
             ps.setString(1, id);
@@ -53,8 +47,9 @@ public class UserDao {
     public void deleteAll() throws SQLException {
         jdbcContextWithStatementStrategy(new DeleteAllStragegy());
     }
+
     public int getCount() throws SQLException {
-        Connection c = connectionMaker.connectionMaker();
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement("select count(*) from users");
         ResultSet rs = ps.executeQuery();
         rs.next();
@@ -63,19 +58,5 @@ public class UserDao {
         ps.close();
         c.close();
         return count;
-    }
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws  SQLException{
-        Connection c = connectionMaker.connectionMaker();
-        PreparedStatement ps = stmt.makePreparedStatement(c);
-        ps.executeUpdate();
-        ps.close();
-        c.close();
-    }
-
-    public static void main(String[] args) {
-        UserDao userDao = new UserDao();
-        userDao.add(new User("9", "Ruru", "1234qwer"));
-        User user = userDao.findById("7");
-        System.out.println(user.getName());
     }
 }
