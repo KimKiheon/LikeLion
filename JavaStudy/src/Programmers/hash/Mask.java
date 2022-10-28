82f2397ad51af7086888e02971d8dab3a70ef0d9
Kimkiheon <cbj2741@gmail.com>
2022-10-28 10:27:16 +0900

    programmers 위장

---
+++ JavaStudy/src/Programmers/hash/Mask.java
@@ -0,0 +1,30 @@
+package Programmers.hash;
+
+
+import java.util.ArrayList;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Mask {
+    class Solution {
+        public int solution(String[][] clothes) {
+            int cnt[] = new int[35];
+            int count = 1, tmp = 1;
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < clothes.length; i++) {
+                list.add(clothes[i][1]);
+            }
+            Collections.sort(list);
+            cnt[0]++;
+            for (int i = 1; i < list.size(); i++) {
+                if (!(list.get(i).equals(list.get(i - 1)))) {
+                    count++;
+                }
+                cnt[count - 1]++;
+            }
+            for (int i = 0; i < count; i++) tmp = tmp * (cnt[i] + 1);
+            return tmp - 1;
+        }
+    }
+}
