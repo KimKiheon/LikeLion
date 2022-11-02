2dbf0aced92aea8447625f210090412fc57d16b1
Kimkiheon <cbj2741@gmail.com>
2022-11-02 09:23:44 +0900

    프로그래머스 소수 찾기 - 에라토스테네스의 체 해결

---
+++ JavaStudy/src/Programmers/_1102/IsPrime.java
@@ -0,0 +1,28 @@
+package Programmers._1102;
+
+public class IsPrime {
+
+
+    public int solution(int n) {
+        int answer = 0;
+        int[] primeNum = new int[1000005];
+        for (int i = 2; i <= n; i++) primeNum[i] = i;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (primeNum[i] == 0) continue;
+            for (int j = i * i; j <= n; j += i) primeNum[j] = 0;
+        }
+        for (int i = 2; i <= n; i++) {
+            if (primeNum[i] == i) answer++;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        IsPrime isPrime = new IsPrime();
+        isPrime.solution(10);
+    }
+}
+
+
