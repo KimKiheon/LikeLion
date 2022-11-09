7abb36c60d29b83c074042d8d8f370f27db5b6b1
Kimkiheon <cbj2741@gmail.com>
2022-11-09 09:08:44 +0900

    Codeup 2083 이분 탐색 완료

--- JavaStudy/src/codeup/_2083.java
+++ JavaStudy/src/codeup/_2083.java
@@ -1,4 +1,29 @@
 package codeup;
 
+import java.util.Scanner;
+
 public class _2083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, key;
+        n=sc.nextInt();
+        key=sc.nextInt();
+        int[] numbers = new int[n];
+        for(int i=0;i<n;i++){
+            numbers[i]=sc.nextInt();
+        }
+        int left = 0, right = numbers.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (numbers[mid] == key) {
+                System.out.println(mid + 1);
+                return;
+            } else if (numbers[mid] < key) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println(-1);
+    }
 }
