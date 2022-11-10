70925cee64bf021141f3187023905a4eea8b55b6
Kimkiheon <cbj2741@gmail.com>
2022-11-10 09:17:17 +0900

    프로그래머스 같은 숫자는 싫어

---
+++ JavaStudy/src/Programmers/_1110/HateSameNumber.java
@@ -0,0 +1,19 @@
+package Programmers._1110;
+import java.util.*;
+
+public class HateSameNumber {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(arr[0]);
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]!=arrayList.get(arrayList.size()-1)) {
+                arrayList.add(arr[i]);
+            }
+        }
+        int[] answer = new int[arrayList.size()];
+        for(int i=0;i<arrayList.size();i++){
+            answer[i]=arrayList.get(i);
+        }
+        return answer;
+    }
+}
