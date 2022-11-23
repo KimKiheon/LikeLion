7f40e021c995d37052b9d99e136b3dfce370a9a9
Kimkiheon <cbj2741@gmail.com>
2022-11-23 10:41:22 +0900

    피보나치 반복문

---
+++ JavaStudy/src/_1123/Fibonacci.java
@@ -0,0 +1,19 @@
+package _1123;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[35];
+        arr[0] = arr[1] = 1;
+        for (int i = 2; i < n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+        }
+        System.out.println(arr[n-1]);
+
+    }
+
+
+}
