34f9ecaa15c4fff23d0c68544ef64e0ba7ba6a2f
Kimkiheon <cbj2741@gmail.com>
2022-11-28 10:04:52 +0900

    실습1, 실습2 재귀함수 'AAA'~'ZZZ

--- JavaStudy/src/_1128/permutationandcombination/AlphabetPrintRecursion.java
+++ JavaStudy/src/_1128/permutationandcombination/AlphabetPrintRecursion.java
@@ -3,30 +3,23 @@ package _1128.permutationandcombination;
 public class AlphabetPrintRecursion {
     static int[] arr = new int[26];
 
-    void function1(int n) {
-        if (n == 26) return;
-        System.out.printf("%c\n", n + 'A');
-        function1(n + 1);
-    }
-
-    void function2(int idx, int n, int depth) {
+    void function(int idx, int n, int depth) {
         if (idx == depth) {
-            for (int i = 0; i < 2; i++) System.out.printf("%c", arr[i] + 'A');
+            for (int i = 0; i < depth; i++) System.out.printf("%c", arr[i] + 'A');
             System.out.println();
             return;
         }
         for (int i = 0; i < n; i++) {
             arr[idx] = i;
-            function2(idx + 1,n,depth);
+            function(idx + 1, n, depth);
             arr[idx] = 0;
         }
-
     }
-
     public static void main(String[] args) {
         AlphabetPrintRecursion alphabetPrintRecursion = new AlphabetPrintRecursion();
 
-        alphabetPrintRecursion.function1(0);
-        alphabetPrintRecursion.function2(0, 26, 2);
+        alphabetPrintRecursion.function(0,26,1);
+        alphabetPrintRecursion.function(0, 26, 2);
+        alphabetPrintRecursion.function(0,26,3);
     }
 }
