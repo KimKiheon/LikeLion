3cb28cbac899b04ddf1959876a5975a873b89d69
Kimkiheon <cbj2741@gmail.com>
2022-11-30 00:23:43 +0900

    Radix Sort with counting sort

--- JavaStudy/src/_1129/RadixSort2.java
+++ JavaStudy/src/_1129/RadixSort2.java
@@ -3,45 +3,46 @@ package _1129;
 import java.util.Arrays;
 
 public class RadixSort2 {
-    static void countSort(int[] arr,int n,int exp) {
-        int buffer[]=new int[n];
-        int counting[]=new int[10];
+    static void countSort(int[] arr, int n, int exp) {
+        int buffer[] = new int[n];
+        int counting[] = new int[10];
 
-        for(int i=0;i<n;i++) {
-            counting[(arr[i]/exp)%10]++;
+        for (int i = 0; i < n; i++) {
+            counting[(arr[i] / exp) % 10]++;
         }
 
-        for(int i=1;i<=9;i++) counting[i]+=counting[i-1];
+        for (int i = 1; i <= 9; i++) counting[i] += counting[i - 1];
 
 
-        for(int i=n-1;i>=0;i--) {
-            int res=arr[i]/exp;
-            res%=10;
-            int idx=counting[res];
-            buffer[idx-1]=arr[i];
+        for (int i = n - 1; i >= 0; i--) {
+            int res = arr[i] / exp;
+            res %= 10;
+            int idx = counting[res];
+            buffer[idx - 1] = arr[i];
             counting[res]--;
         }
 
-        for(int i=0;i<n;i++) {
-            arr[i]=buffer[i];
+        for (int i = 0; i < n; i++) {
+            arr[i] = buffer[i];
         }
     }
 
-    static void RadixSort(int[] arr,int n) {
-        int max=0;
-        for(int i=0;i<n;i++) max=Math.max(max,arr[i]);
+    static void RadixSort(int[] arr, int n) {
+        int max = 0;
+        for (int i = 0; i < n; i++) max = Math.max(max, arr[i]);
 
-        for(int i=1; max/i>0;i*=10) {
-            System.out.println(i+"의 자리에 대해 정렬합니다.");
-            countSort(arr,n,i);
+        for (int i = 1; max / i > 0; i *= 10) {
+            System.out.println(i + "의 자리에 대해 정렬합니다.");
+            countSort(arr, n, i);
             System.out.println("정렬 결과");
             System.out.println(Arrays.toString(arr));
         }
 
     }
+
     public static void main(String[] args) {
-        int[] arr= {23,5,2,17,28, 1, 96, 123, 222};
-        RadixSort(arr,arr.length);
+        int[] arr = {23, 5, 2, 17, 28, 1, 96, 123, 222};
+        RadixSort(arr, arr.length);
         System.out.println("최종 정렬 결과");
         System.out.println(Arrays.toString(arr));
     }
