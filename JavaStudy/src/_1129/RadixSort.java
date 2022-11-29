8d18098ecfba27f6ea82262dc129b40d1283fac2
Kimkiheon <cbj2741@gmail.com>
2022-11-29 10:28:03 +0900

    Radix Sort 자릿수 구하기

--- JavaStudy/src/_1129/RadixSort.java
+++ JavaStudy/src/_1129/RadixSort.java
@@ -1,8 +1,20 @@
 package _1129;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RadixSort {
+    public Set<Integer> digit(int[] arr){
+        Set<Integer> set = new HashSet<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==0) set.add(1);
+            else if (0<arr[i])set.add((int)(Math.floor(Math.log10(arr[i]))+1));
+            else continue;
+        }
+        return set;
+    }
     public static void main(String[] args) {
         int[] arr = new int[]{7, 2, 6, 5, 4, 1};
         int[] radixArr = new int[10];
