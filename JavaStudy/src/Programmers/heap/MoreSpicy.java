b2805dfa6e660a46ed25eb9e7f14458f7523c270
Kimkiheon <cbj2741@gmail.com>
2022-12-07 09:12:55 +0900

    프로그래머스 더 맵게

---
+++ JavaStudy/src/Programmers/heap/MoreSpicy.java
@@ -0,0 +1,17 @@
+package Programmers.heap;
+
+import java.util.PriorityQueue;
+
+public class MoreSpicy {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : scoville) pq.add(s);
+
+        int cnt = 0;
+        while (pq.size() > 1 && pq.peek() < K) {
+            pq.add(pq.remove() + pq.remove() * 2);
+            cnt++;
+        }
+        return pq.peek() >= K ? cnt : -1;
+    }
+}
